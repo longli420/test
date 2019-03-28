bc72e003d032f47d453404e7b4f1944fd25f0e36
longli420 <long_li@dnt.com.cn>
2019-03-28 13:38:20 +0800

    3月26日复习

--- test_0326.py
+++ test_0326.py
@@ -51,3 +51,13 @@ def my_base(x):
         return x
     else:
         return -x
+# ----------------------------
+
+# 获取本地时间
+import time
+# time.localtime(time.time())
+
+
+time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+print(time)
